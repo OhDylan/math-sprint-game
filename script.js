bc0e53fe9eddfe8c79cb6c744c5c122ae73b04e1
Oh Dylan <ddragon1128@hotmail.com>
2021-10-11 10:44:44 +0800

    Added the auto scrolling, and getting right and wrong selection from user

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ const playAgainBtn = document.querySelector('.play-again');
 // Equations
 let questionAmount = 0
 let equationsArray = [];
+let playerGuessArray = []
 
 // Game Page
 let firstNumber = 0;
@@ -30,7 +31,18 @@ const wrongFormat = [];
 
 // Time
 
+
 // Scroll
+let valueY = 0
+
+// Scroll, store user selection in playerGuessArray
+function select(guessedTrue){
+  // Scroll 80 pixel
+  valueY += 80
+  itemContainer.scroll(0, valueY)
+  // Add player guess to array
+  return guessedTrue ? playerGuessArray.push('true') : playerGuessArray.push('false')
+}
 
 // Display Game Page
 function showGamePage(){
